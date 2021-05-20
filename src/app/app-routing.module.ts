0930bc6f9007adf5aca28e48a690f6483ff1d2ed
Marlon Chávez <mchavez-2019249@kinal.edu.gt>
2021-05-19 22:45:40 -0600

    version 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import {HotelComponent} from './components/hotel/hotel.component';
+import {ServicioComponent} from './components/servicio/servicio.component';
+import {SearchHotelComponent} from './components/search-hotel/search-hotel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //una manera de realizarlo
+  {path: '', component: HomeComponent}, //un solo uso | va a ser la primer ruta que se mostrará (primer componente)
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NotFoundComponent},
+  {path: 'hotel', component: HotelComponent},
+  {path: 'servicio', component: ServicioComponent},
+  {path: 'searchH', component: SearchHotelComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//components
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { HotelComponent } from './components/hotel/hotel.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
+import { SearchHotelComponent } from './components/search-hotel/search-hotel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    HotelComponent,
+    ServicioComponent,
+    SearchHotelComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
