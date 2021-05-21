ffa797616c67047f859ad4666321ba265a896742
Erick Barrera <ebarrera-2019263@kinal.edu.gt>
2021-05-21 14:32:43 -0600

    comit para ver si si jalo xd

--- src/app/services/restContact/rest-contact.service.ts
+++ src/app/services/restContact/rest-contact.service.ts
@@ -55,4 +55,13 @@ export class RestContactService {
     .pipe(map(this.extractData))
   }
  
+  holaesteesunejemploparasabersisiguardaelgitkraken(idUser, idContact){
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.restUser.getToken()
+    });
+    return this.http.put(this.uri+idUser+'/removeContact/'+idContact, null, {headers: headers})
+    .pipe(map(this.extractData))
+  }
+
 }
